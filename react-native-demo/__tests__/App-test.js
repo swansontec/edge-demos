d40121487736a42fd670333ce3d9a64be47033b2
William Swanson <swansontec@gmail.com>
2019-11-22 13:08:27 -0800

    Upgrade linting setup

--- react-native-demo/__tests__/App-test.js
+++ react-native-demo/__tests__/App-test.js
@@ -1,14 +1,12 @@
-/**
- * @format
- */
+/* global it */
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import 'react-native'
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import App from '../App.js'
 
 it('renders correctly', () => {
-  renderer.create(<App />);
-});
+  renderer.create(<App />)
+})
--- react-native-demo/index.js
+++ react-native-demo/index.js
@@ -1,9 +1,8 @@
-/**
- * @format
- */
+// @flow
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native'
 
-AppRegistry.registerComponent(appName, () => App);
+import { name as appName } from './app.json'
+import { App } from './src/App.js'
+
+AppRegistry.registerComponent(appName, () => App)
